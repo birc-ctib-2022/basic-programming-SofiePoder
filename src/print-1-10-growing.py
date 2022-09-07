d68cfd2b16c07651139f32fca831e7f9264c3e35
SofiePoder <201905630@post.au.dk>
2022-09-07 20:15:45 +0200

    I fixed the spaces

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -1,9 +1,12 @@
 
 # Print the numbers described in the exercise
-
 list = " "
 for i in range(10):
-    list += " "
-    list += str(i+1)
-    print(list)
-  
+    if i != 10:
+        list += str(i+1)
+        list += " "
+        print(list)
+    else:
+        list += str(i+1)
+        
+      
